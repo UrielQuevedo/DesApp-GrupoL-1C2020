57e3658fc39e0f2c3910772d06a807c88dd0b26a
Daniel Villegas <dani.villegascarp@gmail.com>
2020-04-26 04:11:23 -0300

    Se agregan datos basicos de un order

--- backend/src/main/java/unq/ar/edu/dessap/grupol/model/Order.java
+++ backend/src/main/java/unq/ar/edu/dessap/grupol/model/Order.java
@@ -1,6 +1,48 @@
 package unq.ar.edu.dessap.grupol.model;
 
+import java.util.List;
+
 public class Order {
 
+    private long id;
+    private Buyer buyer;
+    private List<Product> products;
+    private List<Store> stores;
+
+    public Order() {}
+
+    public Order(Buyer buyer, List<Product> products, List<Store> stores) {
+        this.buyer = buyer;
+        this.products = products;
+        this.stores = stores;
+    }
+
+    public List<Store> getStores() {
+        return stores;
+    }
+
+    public void setStores(List<Store> stores) {
+        this.stores = stores;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public Buyer getBuyer() {
+        return buyer;
+    }
+
+    public void setBuyer(Buyer buyer) {
+        this.buyer = buyer;
+    }
+
+    public long getId() {
+        return id;
+    }
 
 }
