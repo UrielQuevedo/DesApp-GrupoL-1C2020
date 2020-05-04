f30ad79a4ef80707a99b6205c56166ce8e8f36f5
urielquevedo <quevedouriel3@gmail.com>
2020-05-04 16:56:45 -0300

    add cors

--- backend/src/main/java/unq/ar/edu/dessap/grupol/GrupolApplication.java
+++ backend/src/main/java/unq/ar/edu/dessap/grupol/GrupolApplication.java
@@ -2,9 +2,16 @@ package unq.ar.edu.dessap.grupol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class GrupolApplication {
+public class GrupolApplication implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GrupolApplication.class, args);
