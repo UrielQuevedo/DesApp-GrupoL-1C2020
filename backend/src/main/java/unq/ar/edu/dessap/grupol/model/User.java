3893ed5d70126c5be8c5c56da451bba6a5a346e8
Daniel Villegas <dani.villegascarp@gmail.com>
2020-05-07 22:29:56 -0300

    Se agrega test y builder de user

--- backend/src/main/java/unq/ar/edu/dessap/grupol/model/User.java
+++ backend/src/main/java/unq/ar/edu/dessap/grupol/model/User.java
@@ -5,13 +5,25 @@ public class User {
     private long id;
     private String username;
     private String password;
+    private String email;
     private String role;
 
-    public User(long id, String username, String password, String role) {
+    public User() {}
+
+    public User(long id, String username, String password, String email, String role) {
         this.id = id;
         this.username = username;
         this.password = password;
+        this.email = email;
         this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getRole() {
---
+++ backend/src/test/java/unq/ar/edu/dessap/grupol/model/UserTest.java
@@ -0,0 +1,50 @@
+package unq.ar.edu.dessap.grupol.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+import unq.ar.edu.dessap.grupol.service.builder.UserBuilder;
+
+public class UserTest {
+
+    @Test
+    public void testGivenAUserWithIdWhenTheyReceiveGetIdThenGiveHisId() {
+        User user = UserBuilder.aUser()
+                     .withId(1).build();
+
+        Assert.assertEquals(1, user.getId());
+    }
+
+    @Test
+    public void testGivenAUserWithUsernameWhenTheyReceiveGetIdThenGiveHisUsername() {
+        User user = UserBuilder.aUser()
+                     .withUsername("daniv").build();
+
+        Assert.assertEquals("daniv", user.getUsername());
+    }
+
+    @Test
+    public void testGivenAUserWithPasswordWhenTheyReceiveGetPasswordThenGiveHisPassword() {
+        User user = UserBuilder.aUser()
+                     .withPassword("123").build();
+
+        Assert.assertEquals("123", user.getPassword());
+    }
+
+    @Test
+    public void testGivenAUserWithEmailWhenTheyReceiveGetEmailThenGiveHisEmail() {
+        User user = UserBuilder.aUser()
+                    .withEmail("dvillegas@gmail.com").build();
+
+        Assert.assertEquals("dvillegas@gmail.com", user.getEmail());
+    }
+
+
+    @Test
+    public void testGivenAUserWithRoleBuyerWhenTheyReceiveGetRoleThenGiveHisRole() {
+        User user = UserBuilder.aUser()
+                    .withRole("Buyer").build();
+
+        Assert.assertEquals("Buyer", user.getRole());
+    }
+
+}
