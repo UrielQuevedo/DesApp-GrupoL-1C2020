28bc971b1f4f9b1e0933665c5124e4636d7e7684
Daniel Villegas <dani.villegascarp@gmail.com>
2020-05-07 22:09:44 -0300

    Se agregan datos basicos del user

---
+++ backend/src/main/java/unq/ar/edu/dessap/grupol/model/User.java
@@ -0,0 +1,48 @@
+package unq.ar.edu.dessap.grupol.model;
+
+public class User {
+
+    private long id;
+    private String username;
+    private String password;
+    private String role;
+
+    public User(long id, String username, String password, String role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
