6ac0a80e0a8c36f585eae6b587c225affc239710
urielquevedo <quevedouriel3@gmail.com>
2020-05-21 23:52:26 -0300

    Agrego el layout para mostrar los rubros

--- frontend/src/View/Home.js
+++ frontend/src/View/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Grid, TextField, Button, InputAdornment } from '@material-ui/core';
+import { Grid, TextField, Button, InputAdornment, Box } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import '../Styles/Home.css';
 
-
 const Home = () => {
+
+  const categories = [
+    { url: 'https://res.cloudinary.com/dddzzcrzg/image/upload/v1590114819/carniceria_qehdne.jpg', name: 'carniceria' },
+    { url: 'https://res.cloudinary.com/dddzzcrzg/image/upload/v1590114822/farmacia_oz38xp.jpg', name: 'farmacia' },
+    { url: 'https://res.cloudinary.com/dddzzcrzg/image/upload/v1590114819/verduleria_qpyzqb.jpg', name: 'verduleria' },
+    { url: 'https://res.cloudinary.com/dddzzcrzg/image/upload/v1590114819/almacen_nksrfp.jpg', name: 'almacen' },
+    { url: 'https://res.cloudinary.com/dddzzcrzg/image/upload/v1590114820/kiosco_icwjb0.jpg', name: 'kiosco' },
+    { url: 'https://res.cloudinary.com/dddzzcrzg/image/upload/v1590114819/dietetica_zcwr0l.jpg', name: 'dietetica' },
+  ]
 
   const SearchLayout = () => {
     return (
@@ -40,9 +48,31 @@ const Home = () => {
   }
 
   const StoreCategoriesList = () => {
+    return categories.map(({ url, name }, i) => (
+      <div className="item-categorie mt-20" key={i}>
+        <Box boxShadow={4} className="image-categorie">
+          <img
+            src={url}
+            alt={"Categoria de " + name}
+          />
+        </Box>
+        <p>{name}</p>
+      </div>
+    ));
+  }
+
+  const StoreCategories = () => {
     return (
-      <div>
-        RUBROS
+      <div className="mt-20 storeCategories">
+        <span className="title">
+          Nuestros rubros
+        </span>
+        <p className="subtitle" >
+          Lo mas cercano de tu casa
+        </p>
+        <Grid container direction="row">
+          <StoreCategoriesList />
+        </Grid>
       </div>
     );
   }
@@ -58,7 +88,7 @@ const Home = () => {
   return (
     <>
       <SearchLayout />
-      <StoreCategoriesList />
+      <StoreCategories />
       <Information />
     </>
   );
