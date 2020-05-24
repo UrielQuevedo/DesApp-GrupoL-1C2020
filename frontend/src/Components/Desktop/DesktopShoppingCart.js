f63c6cf5a2891a71e1a43b7f1c456e0a13643fa1
urielquevedo <quevedouriel3@gmail.com>
2020-05-23 22:14:45 -0300

    Creo el componente del button shopping cart para desktop

---
+++ frontend/src/Components/Desktop/DesktopShoppingCart.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Box, Badge } from '@material-ui/core';
+import '../../Styles/ShoppingCart.css';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { useState } from 'react';
+
+const DesktopShoppingCart = () => {
+  // correct, alert, danger son estados del color del boton dependiendo de si supera lo establecido
+  const [state, setstate] = useState("correct");
+
+  return (
+    <Box className={"button " + state} boxShadow={3}>
+      <Badge badgeContent={3} className="shoppingCart">
+        <ShoppingCartIcon style={{color:'#ffff'}} />
+      </Badge>
+      <p className="price">10000 $</p>
+    </Box>
+  );
+}
+
+export default DesktopShoppingCart;
