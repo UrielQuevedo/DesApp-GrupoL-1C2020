7655db15562acae08d14b098f5a7acfe469a8a69
urielquevedo <quevedouriel3@gmail.com>
2020-05-27 04:51:19 -0300

    Mejoro la view de location, y agrego leaflet

--- frontend/src/View/Location.js
+++ frontend/src/View/Location.js
@@ -1,134 +1,139 @@
 import React, { useState, useEffect } from 'react';
 import LocationOn from '@material-ui/icons/LocationOn';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
-import { Typography, Box, Grid, Button, TextField, InputAdornment, CircularProgress } from '@material-ui/core';
-import Autocomplete from '@material-ui/lab/Autocomplete';
-import Axios from 'axios';
+import { Box, Grid, Button, CircularProgress } from '@material-ui/core';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import '../Styles/Location.css';
-import { useHistory } from 'react-router';
-import ErrorIcon from "@material-ui/icons/Error";
-import { sendBuyerLocationRequest } from '../Service/Api';
+import { Map, Popup, TileLayer, Marker } from 'react-leaflet';
+import { useRef } from 'react';
+import { useHereMapService } from '../Service/HereMapService';
 
 const Location = () => {
-  const [ options, setOptions ] = useState([]);
-  const [ isValid, setIsValid ] = useState(true);
-  const [ loading, setLoading ] = useState(false);
-  const [ currentLocation, setCurrentLocation ] = useState({});
-  const apikey = 'A5liKoRj88kze2rtyInYq-m6Eah7Hkg3Z2bd4kQoBH4';
-  const { push } = useHistory();
+  const refMarker = useRef();
+  const [ isLocationChangeView, setIsLocationChangeView ] = useState(false);
+  const [ actualCoords, setActualCoords ] = useState({});
+  const { loading, location, fetchPositionsByCoords } = useHereMapService();
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(({ coords }) => {
-      setLoading(true);
-      Axios.get(`https://revgeocode.search.hereapi.com/v1/revgeocode?at=${coords.latitude}%2C${coords.longitude}&apiKey=${apikey}`)
-      .then((locations) => setCurrentLocation({ address: locations.data.items[0].address.label }))
-      .catch((_) => setIsValid(false))
-      .finally((_) => setLoading(false));
+      setActualCoords({ lat: coords.latitude, lng: coords.longitude });
     });
   }, [])
 
-  const changeOptionLocation = (e) => {
-    const locationAddress = e.target.value;
-    e.preventDefault();
-    setLoading(true);
-    Axios.get(`https://geocode.search.hereapi.com/v1/geocode?q=${locationAddress}&in=countryCode:ARG&apiKey=${apikey}`)
-      .then((locations) => {
-        const { items } = locations.data;
-        setOptions(items);
-        const { position } = items[0];
-        position.lat !== undefined && position.lng !== undefined ? setIsValid(true) : setIsValid(false);
-      })
-      .catch((_) => {
-        setCurrentLocation({ address: locationAddress });
-        setIsValid(false)
-      })
-      .finally((_) => setLoading(false));
+  // const sendLocation = async (e) => {
+  //   TODO try catch falta
+  //   TODO enviar hacia el user
+  //   const user = await sendBuyerLocationRequest(1, location);
+  //   push('/')
+  // }
+
+  const handlerIsChangeLocation = () => {
+    setIsLocationChangeView(true);
   }
 
-  const sendLocation = async (e) => {
-    //TODO try catch falta
-    e.preventDefault();
-    setLoading(true);
-    const locations = await Axios.get(`https://geocode.search.hereapi.com/v1/geocode?q=${currentLocation.address}&in=countryCode:ARG&apiKey=${apikey}`)
-    const { address, position } = locations.data.items[0];
-    const location = ({ address: address.label, latitude: position.lat, longitude: position.lng });
-    console.log(location);
-    //TODO enviar hacia el user
-    //const user = await sendBuyerLocationRequest(1, location);
-    //push('/')
+  const handlerUpdatePosition = () => {
+    const marker = refMarker.current;
+    if (marker != null) {
+      setActualCoords(marker.leafletElement.getLatLng());
+    };
+  }
+
+  const LocationView = () => {
+    const isDisabled = !Boolean(location.address);
+    return (
+      <>
+        <Grid
+          container
+          justify="center"
+        >
+          <Button className="location-button" onClick={handlerIsChangeLocation}>
+            <LocationOn />
+              <Box textOverflow="ellipsis" overflow="hidden" className="location-address">
+                { location?.address || 'ingresar dirección' }
+              </Box>
+            <ArrowDropDownIcon />
+          </Button>
+        </Grid>
+        <Grid container justify="center">
+          <p className="description-location">
+            Ingrese una dirección para poder continuar
+          </p>
+        </Grid>
+        <Grid container justify="flex-end" className="continue-button-container">
+          <Button
+            size="small"
+            type="submit"
+            disabled={isDisabled}
+            variant="contained"
+            className={isDisabled ? 'mt-30' : 'continue-button'}
+          >
+            continuar
+            <ArrowForwardIosIcon className="arrow-icon" />
+          </Button>
+        </Grid>
+      </>
+    );
+  }
+
+  const handlerChangeLocation = () => {
+    fetchPositionsByCoords(actualCoords, () => setIsLocationChangeView(false));
+  }
+
+  const LocationChangeView = () => {
+    return (
+      <>
+        <Map center={actualCoords} zoom={16}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker
+            position={actualCoords}
+            draggable={true}
+            ondragend={handlerUpdatePosition}
+            ref={refMarker}
+          >
+            <Popup minWidth={90}>
+              <span>
+                TU UBICACION
+              </span>
+            </Popup>
+          </Marker>
+        </Map>
+        <Grid container justify="flex-end" className="accept-button-container">
+          { loading && <CircularProgress className="circularProgress-pink" /> }
+          <Button
+            size="small"
+            type="submit"
+            disabled={loading}
+            variant="contained"
+            className={loading ? 'mt-30' : 'mt-30 validated-button'}
+            endIcon={<ArrowForwardIosIcon className="arrow-icon" />}
+            onClick={handlerChangeLocation}
+          >
+            aceptar
+          </Button>
+        </Grid>
+      </>
+    );
   }
 
   return (
-    <Grid
-      container
-      direction="column"
-      justify="center"
-      alignItems="center"
-      style={{ marginTop:'10vh' }}
-    >
-      <form onSubmit={sendLocation}>
+    <Grid container justify="center" alignItems="center" style={{ height:'100vh' }}>
+      <div className="color-strip" />
         <Box
-          padding="40px"
           borderRadius="12px"
-          item="true"
-          className="location-form"
-          xs={6}
+          padding="40px"
+          item
+          boxShadow={4}
+          textAlign="center"
+          className="location-container"
         >
-          <Typography className="title">
-            Seleccionar una dirección de entrega
-          </Typography>
-          <Grid justify="center" container>
-            <Grid item lg={6}>
-              <Grid container direction="row" style={{ position:'relative' }}>
-                <Autocomplete
-                  id="free-solo-demo"
-                  freeSolo
-                  name="location"
-                  value={currentLocation.address || ''}
-                  style={{ width:'100%', marginLeft:'10px' }}
-                  required
-                  options={options.map((option) => option.address.label)}
-                  renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      style={{ width:'100%'}}
-                      placeholder="Calle Numero, CPostal Partido, Pais"
-                      margin="normal"
-                      onChange={(e) => changeOptionLocation(e)}
-                      variant="outlined"
-                      name="location"
-                      required
-                      InputProps={{
-                        ...params.InputProps,
-                        startAdornment: (
-                            <InputAdornment position="start">
-                              { isValid && !loading && <LocationOn style={{ color:'#4EEA5D' }}/> }
-                              { !isValid && !loading && <ErrorIcon style={{ color:'red' }}/> }
-                              { loading && <CircularProgress style={{color:'blue'}} size={20}/> }
-                            </InputAdornment>
-                        )
-                      }}
-                    />
-                  )}
-                />
-              </Grid>
-              <Typography className="description">
-                Por favor seleccione una direcion para poder continuar, luego esta podra ser modificada.
-              </Typography>
-            </Grid>
-          </Grid>
-          <Button
-            size="medium"
-            type="submit"
-            disabled = {!isValid}
-            className={ isValid ? 'validated' : 'notValidated' }
-            variant="contained"
-            endIcon={<ArrowForwardIosIcon />}
-          >
-            continuar
-          </Button>
+          <h1>
+            Dirección de entrega
+          </h1>
+          { isLocationChangeView ? <LocationChangeView /> : <LocationView /> }
         </Box>
-      </form>
     </Grid>
   );
 }
