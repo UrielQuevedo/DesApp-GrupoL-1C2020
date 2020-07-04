6795d4b443e746ce6df5de84ce45dbc8587ddd39
Daniel Villegas <dani.villegascarp@gmail.com>
2020-07-04 16:25:55 -0300

    Se agrega variable de entorno client id

--- frontend/src/Utils/AuthGoogleLogin.js
+++ frontend/src/Utils/AuthGoogleLogin.js
@@ -4,20 +4,22 @@ import '../Styles/ButtonGoogle.css';
 
 const AuthGoogleLogin = () => {
 
+    const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+
     const responseGoogle = (response) => {
         console.log(response);
     }
 
     return (
         <div>
-                <GoogleLogin
-                clientId="561793782261-01t0p7n71thmbqqum17ovu537cm3881m.apps.googleusercontent.com"
-                buttonText="Iniciar sesión con Google"
-                onSuccess={responseGoogle}
-                onFailure={responseGoogle}
-                cookiePolicy={'single_host_origin'}
-                className="buttonGoogle"
-                />
+            <GoogleLogin
+            clientId={clientId}
+            buttonText="Iniciar sesión con Google"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={'single_host_origin'}
+            className="buttonGoogle"
+            />
         </div>
     )
 }
