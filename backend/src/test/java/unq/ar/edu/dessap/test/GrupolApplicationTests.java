6a5cde14331342d021544139affe12638a3d75c9
Dani Villegas <dani.villegascarp@gmail.com>
2020-07-12 22:43:55 -0300

    Fixed test

--- backend/src/test/java/unq/ar/edu/dessap/test/GrupolApplicationTests.java
+++ backend/src/test/java/unq/ar/edu/dessap/test/GrupolApplicationTests.java
@@ -2,8 +2,9 @@ package unq.ar.edu.dessap.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+@ContextConfiguration
 class GrupolApplicationTests {
 
     @Test
